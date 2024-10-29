f37c90289aad5f2bb8f8ea57b0e921c832dcb131
monkgr74 <varun007.dasari@gmail.com>
2024-10-29 17:16:15 -0400

    Balh Blah

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/strafeTest.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class strafeTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DriveTrain drivetrain = new DriveTrain(this);
+
+        waitForStart();
+
+        if(isStopRequested()) {
+            return;
+        }
+
+        while(opModeIsActive()) {
+            double x = gamepad1.left_stick_x;
+
+            double frontLeftPower = (-x);
+            double backLeftPower = (x);
+            double frontRightPower = (x);
+            double backRightPower = (-x);
+
+            drivetrain.frontLeft.setPower(frontLeftPower);
+            drivetrain.frontRight.setPower(frontRightPower);
+            drivetrain.backLeft.setPower(backLeftPower);
+            drivetrain.backRight.setPower(backRightPower);
+            telemetry.addData("Front Left Power", frontLeftPower);
+            telemetry.addData("Front right power", frontRightPower);
+            telemetry.addData("back Left Power", backLeftPower);
+            telemetry.addData("back right Power", backRightPower);
+            telemetry.update();
+
+        }
+    }
+
+}
