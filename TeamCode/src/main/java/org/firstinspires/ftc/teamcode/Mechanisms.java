fe01e0186bb0ddbde5d34bb82f9ad097c5cb9bf8
aryz-ssh <ajalvarez3099@gmail.com>
2024-10-29 17:08:04 -0400

    here i added some claw code in the mechanisms file. the servo thresholds need to be adjusted because i have no idea what servos we're even using 🕊️

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms.java
@@ -9,10 +9,21 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Mechanisms {
     public DcMotor viperSlide;
     public DcMotor linearSlide;
-    public Servo claw;
+    public Servo clawLeft;
+    public Servo clawRight;
+    public Servo clawMesh;
+    // public Servo claw;
 
    // public double open = 0.83;
    // public double close = 0.5;
+
+    // Claw positions
+    private final double CLAW_LEFT_OPEN = 0.8;
+    private final double CLAW_LEFT_CLOSED = 0.5;
+    private final double CLAW_RIGHT_OPEN = 0.2;
+    private final double CLAW_RIGHT_CLOSED = 0.5;
+    private final double CLAW_MESH_EXTENDED = 1.0;
+    private final double CLAW_MESH_RETRACTED = 0.0;
 
     int maxPosition = 4395;
     double slideSpeed = 0.6;
@@ -52,6 +63,40 @@ public class Mechanisms {
         viperSlide.setTargetPosition(pos1);
         viperSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         viperSlide.setPower(slideSpeed);
+    }
+
+    // Initialize claw servos
+    public void initClaw(HardwareMap hardwareMap) {
+        clawLeft = hardwareMap.get(Servo.class, "clawLeft");
+        clawRight = hardwareMap.get(Servo.class, "clawRight");
+        clawMesh = hardwareMap.get(Servo.class, "clawMesh");
+
+        // Set the initial position of the servos
+        setClawPosition(CLAW_LEFT_CLOSED, CLAW_RIGHT_CLOSED);  // Start closed
+        setClawMeshPosition(CLAW_MESH_RETRACTED);  // Start retracted
+    }
+
+    // Set claw open/close position based on input values
+    public void setClawPosition(double leftPosition, double rightPosition) {
+        clawLeft.setPosition(Math.max(CLAW_LEFT_CLOSED, Math.min(leftPosition, CLAW_LEFT_OPEN)));
+        clawRight.setPosition(Math.max(CLAW_RIGHT_CLOSED, Math.min(rightPosition, CLAW_RIGHT_OPEN)));
+    }
+
+    // Set claw extend/retract position based on input value
+    public void setClawMeshPosition(double meshPosition) {
+        clawMesh.setPosition(Math.max(CLAW_MESH_RETRACTED, Math.min(meshPosition, CLAW_MESH_EXTENDED)));
+    }
+
+    // Helper methods for incrementing/decrementing positions for finer control
+    public void incrementClawPosition(double increment) {
+        double newLeftPosition = clawLeft.getPosition() + increment;
+        double newRightPosition = clawRight.getPosition() + increment;
+        setClawPosition(newLeftPosition, newRightPosition);
+    }
+
+    public void incrementClawMeshPosition(double increment) {
+        double newMeshPosition = clawMesh.getPosition() + increment;
+        setClawMeshPosition(newMeshPosition);
     }
 /*
     public void BasketScorePosition() {
